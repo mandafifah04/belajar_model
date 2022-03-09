59a7a0ae66b9f0938182565f42aa9b70797ae775
= <=khofifahmanda04@gmail.com>
2022-03-10 06:02:24 +0700

    first commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index ()
+    {
+        $news =News::all();
+       return view('welcome',compact ('news'));
+    }
+    //
+}
